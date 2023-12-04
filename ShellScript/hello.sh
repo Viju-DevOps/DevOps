70d8b347e77419bab0eab126ba3677713e7128ba
Vijudev V S <99727626+Viju-DevOps@users.noreply.github.com>
2023-12-04 18:25:26 +0530

    Update hello.sh

--- ShellScript/hello.sh
+++ ShellScript/hello.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
 
+# Variable definition for DB backup
+
+DB_NAME="devops_db"
+DB_USER="root"
+DB_HOST="localhost"
+NOW=$(date +%F_%H-%M-%S)
+BACKUP_FILE="$DB_NAME-$NOW"
+BACKUP_PATH="/var/tmp"
+
+
+# Commands to Perform the DB Backup 
+
+sudo mysqldump -u "$DB_USER" -h  "$DB_HOST" "$DB_NAME" > "$BACKUP_PATH/$BACKUP_FILE".sql
+
+echo "Backup Completed"
